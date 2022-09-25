2f9180284bd054a1d8dc26967438575010defd14
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-09-25 17:27:59 -0300

    Update musics.py
    Já temos um dataframe com todas as informações de todas as músicas e um json com a letra de todas as musicas. Falta so organizar o json para ter a info de qual album é a musica

--- musics.py
+++ musics.py
@@ -1,6 +1,10 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
 
 #este é o meu token de acesso para poder utilizar a api do spotify
 spot = spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials(client_id="4122b8a894694d3e8cf1d8a19cf93aec", client_secret="665e23d198b1458f8a4ce02303100b3d"))
@@ -11,7 +15,7 @@ esta linha linha de código talvez seja útil depois:
 artist_top_tracks(artist_id, country='US')'''
 
 #no spotify, cada artista tem uma id para poder reconhecê-lo no sistema
-artist_id = '3ZHU5AKrUmIPnCFfr82QER'
+artist_id = '0du5cEVh5yTK9QJze8zA0C'
     
 #devolve um dicionário com a id do album e o nome dele para cada album do artista
 def get_all_albuns(id_artista):
@@ -43,8 +47,59 @@ def get_album_data(album_id, album_name):
     colunas = pd.MultiIndex.from_tuples(tuple_columns)
     return pd.DataFrame(data, columns =colunas, index=['duração(seg)','popularidade'])
 
-albuns = get_all_albuns('5j4HeCoUlzhfWtjAfM1acR')
-dfs ={}
-for id, nome in albuns.items():
-    dfs[nome]= get_album_data(id, nome)
-print(pd.concat(dfs.values(), axis=1))
+#A url segue o seguinte padrão: "https://genius.com/nome-do-artista-nome-da-música-lyrics"
+def formatar_para_url(nome_da_musica):
+    
+    # algumas musicas do album "An evening with silk sonic" do Bruno Mars
+    # aparecem no genius como musicas do silk song
+    chave = 0
+    if nome_da_musica in ['Silk Sonic Intro', 'Leave The Door Open', 'Fly As Me', 'After Last Night (with Thundercat & Bootsy Collins)',
+                            'Smokin Out The Window', 'Put On A Smile', '777', 'Skate', 'Blast Off']:
+        chave = 1
+    
+    #este trecho limpa pedaços como (feat. Damian Marley), deixando apenas o nome da musica
+    if len(re.findall(r'\([^)]*\)', nome_da_musica)) != 0:
+        nome_da_musica = re.sub(r'\([^)]*\)', "", nome_da_musica)
+        nome_da_musica = nome_da_musica[:-1]
+    
+    nome_da_musica = nome_da_musica.replace("&", "and").replace(" ", "-").lower().replace("'", "")
+    
+    if chave == 1:
+        return f'https://genius.com/Silk-sonic-{nome_da_musica}-lyrics'
+    return f'https://genius.com/Bruno-mars-{nome_da_musica}-lyrics'
+
+def get_lyrics(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(requests.get(url).content, 'lxml')
+    letra=''
+    for tag in soup.select('div[class^="Lyrics__Container"], .song_body-lyrics p'):
+        t = tag.get_text(strip=True, separator='\n')
+        if t:
+            letra+=t
+    return letra
+
+#criar um dataframe com todas as informações de todas as musicas
+def create_dataframe():
+    albuns = get_all_albuns(artist_id)
+    dfs ={}
+    for id, nome in albuns.items():
+        dfs[nome]= get_album_data(id, nome)
+    df = pd.concat(dfs.values(), axis=1)
+
+    #tirando essa delux edition para nao ter músicas repetidas
+    df.drop('Unorthodox Jukebox (Deluxe Edition)', axis=1, inplace= True, level=0)
+    
+    #df.to_excel(r'dataframe.xlsx')
+    return df
+
+#cria um json com a letra de todas as músicas
+def get_all_lyrics():
+    df = create_dataframe()
+    musicas = {}
+    for i in df:
+        url = formatar_para_url(i[1])
+        letra = get_lyrics(url)
+        musicas[i[1]] = letra
+    with open("musicas.json", "w") as outfile:
+        json.dump(musicas, outfile)
+get_all_lyrics()
