08bafdc37c028f319894f624a88749ae646e0481
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-09-30 14:02:57 -0300

    Update preguica_main.py

--- preguica_main.py
+++ preguica_main.py
@@ -1,29 +1,48 @@
 import musics
 import pandas as pd
 import json
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import nltk
 from nltk.corpus import stopwords
-import  re
 
 global df, albuns
-df = pd.read_csv('dataframe.csv', index_col=0).reset_index()#manter essa linha se quiser o dataframe vindo do arquivo csv
-df = df.set_index(['Álbuns', 'Músicas'])
+
+#diminui tempo de execução ao usar df vindo do cdv
+# df = pd.read_csv('dataframe.csv', index_col=0).reset_index()
+# df = df.set_index(['Álbuns', 'Músicas'])
+
+df = musics.create_dataframe()
+
 albuns = df.index.levels[0].values
 
+#criando dois novos dataframes para poder pegar a soma das premiações de cada música em cada álbum
+df2 = df.iloc[:, 13]
+premios = []
+for album in list(albuns):
+    j = df2.loc[album].sum()
+    premios.append([j])
+df3 = pd.DataFrame(premios, index=list(albuns), columns=["prêmios"])
+
 def grupo_um():
+    #pegando as músicas mais e menos tocadas e com mais e menos duração por álbum
     for album in list(albuns):
         i_max = df.loc[album].idxmax()
         i_min = df.loc[album].idxmin()
-        print("Música com mais duração, popularidade e prêmios do álbum ",album,":","\n",i_max,"\n",sep="")
-        print("Música com menos duração, popularidade e prêmios do álbum ",album,":","\n",i_min,"\n",sep="")
-
+        print("Música com mais duração e popularidade do álbum ",album,":","\n",i_max[[0,1]],"\n",sep="")
+        print("Música com menos duração e popularidade do álbum ",album,":","\n",i_min[[0,1]],"\n",sep="")
+    #pegando as músicas mais e menos tocadas e com mais e menos duração em toda a discografia
     ii_max = df.loc[albuns].idxmax()
-    print("Música com mais duração, popularidade e prêmios em toda a discografia:", "\n", ii_max, "\n", sep="")
+    print("Música com mais duração e popularidade em toda a discografia:", "\n", ii_max[[0,1]], "\n", sep="")
     ii_min = df.loc[albuns].idxmin()
-    print("Música com menos duração, popularidade e prêmios em toda a discografia:", "\n", ii_min, "\n", sep="")
+    print("Música com menos duração e popularidade em toda a discografia:", "\n", ii_min[[0,1]], "\n", sep="")
+    #pegando as premiações por álbum e o álbum mais premiado
+    v = df3["prêmios"].idxmax()
+    print("Álbum com mais premiações:", v)
 
-
-# palavras mais comuns nas letras das musicas por album e em toda discografia
+#palavras mais comuns nas letras das musicas por album e em toda discografia
 def palavras_comuns_musicas():
     freq_total = pd.Series(0)
     for album in albuns:
@@ -34,7 +53,6 @@ def palavras_comuns_musicas():
 
 # frequencia das palavras nos títulos dos albuns
 # print(musics.count_freq(df.index.levels[0].values).head(3))
-
 
 # frequencia das palavras nos titulos das musicas
 # print(musics.count_freq(df.index.levels[1].values))
@@ -81,3 +99,46 @@ def titulo_musica_na_letra():
                     #esse é o caso de uma palavra chave da música não ter nenhuma ocorrência na letra da música
         return ocorrencias
 
+
+def plot():
+    #criando uma paleta de cor para usar como diferenciação das músicas por seus respectivos álbuns
+    custom_palette = []
+    albuns = df.index.levels[0].values
+    for i in df.index.values:
+        if i[0] == albuns[0]:
+            custom_palette.append('k')
+        elif i[0] == albuns[1]:
+            custom_palette.append('y')
+        elif i[0] == albuns[2]:
+            custom_palette.append('g')
+        elif i[0] == albuns[3]:
+            custom_palette.append('r')
+    #criando as cores para a legenda dos gráficos
+    primeiro = mpatches.Patch(color='k', label=albuns[0])
+    segundo = mpatches.Patch(color='y', label=albuns[1])
+    terceiro = mpatches.Patch(color='g', label=albuns[2])
+    quarto = mpatches.Patch(color='r', label=albuns[3])
+    cor_leg = [primeiro, segundo, terceiro, quarto]
+
+    #criação dos gráficos
+    sns.set(style = 'whitegrid')
+
+    sns.barplot(x = df.index.levels[1].values, y = 'popularidade', data=df, palette=custom_palette)
+    plt.xticks(fontsize=7, rotation=80)
+    plt.title('Popularidade das músicas', fontsize=15)
+    plt.ylabel('Popularidade', fontsize=10)
+    plt.legend(handles=cor_leg)
+    plt.show()
+
+    sns.barplot(x = df.index.levels[1].values, y = 'duração(seg)', data=df, palette=custom_palette)
+    plt.xticks(fontsize=7, rotation=80)
+    plt.title('Durações das músicas', fontsize=15)
+    plt.ylabel('Duração(seg)', fontsize=10)
+    plt.legend(handles=cor_leg)
+    plt.show()
+
+    sns.barplot(data = df3, x = df3.index.values ,y = "prêmios")
+    plt.title('Prêmios dos álbuns', fontsize=15)
+    plt.xlabel('Álbuns', fontsize=10)
+    plt.ylabel('Prêmios', fontsize=10)
+    plt.show()
