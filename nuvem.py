1d6ac7fc4d02d5c805aa811009b071ad16dbca26
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-09-30 17:55:29 -0300

    Delete nuvem.py

--- nuvem.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-
-wordcloud = WordCloud(background_color="white", colormap='Dark2')
-wordcloud.generate_from_frequencies(frequencies=serie)
-plt.figure()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
