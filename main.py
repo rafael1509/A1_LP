fed05db4e593bfb0361ec86495f58303b6e6371f
João Victor Pereira Oliveira <69165032+JoaoPereiraOliveira@users.noreply.github.com>
2022-10-01 11:36:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing.resource_sharer import stop
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import musics
@@ -11,21 +12,19 @@ import matplotlib.patches as mpatches
 from nltk.corpus import stopwords
 
 global df, albuns
-# df = musics.create_dataframe()
+#df = musics.create_dataframe()
 
 #diminui tempo de execução ao usar df vindo do csv
 df = pd.read_csv('dataframe.csv', index_col=0, encoding='utf-8-sig', sep='\s*,\s*', engine='python').reset_index()
 df = df.set_index(['Álbuns', 'Músicas'])
-
-
 albuns = df.index.levels[0].values
 
 
 #criando dois novos dataframes para poder pegar a soma das premiações de cada música em cada álbum
-df2 = df.iloc[:, 13]
+#df2 = df.iloc[:, 13]
 premios = []
-for album in list(albuns):
-    j = df2.loc[album].sum()
+for album in albuns:
+    j = df.loc[album].iloc[:, 13].sum()
     premios.append([j])
 df3 = pd.DataFrame(premios, index=list(albuns), columns=["prêmios"])
 
@@ -44,6 +43,8 @@ def grupo_um():
     #pegando as premiações por álbum e o álbum mais premiado
     v = df3["prêmios"].idxmax()
     print("Álbum com mais premiações:", v)
+
+#grupo_um()
 
 
 #palavras mais comuns nas letras das musicas por album e em toda discografia
@@ -140,50 +141,6 @@ def titulo_musica_na_letra():
         return ocorrencias
 
 
-#plotar gráficos referentes ao grupo 1
-def plot():
-    #criando uma paleta de cor para usar como diferenciação das músicas por seus respectivos álbuns
-    custom_palette = []
-    albuns = df.index.levels[0].values
-    for i in df.index.values:
-        if i[0] == albuns[0]:
-            custom_palette.append('k')
-        elif i[0] == albuns[1]:
-            custom_palette.append('y')
-        elif i[0] == albuns[2]:
-            custom_palette.append('g')
-        elif i[0] == albuns[3]:
-            custom_palette.append('r')
-    #criando as cores para a legenda dos gráficos
-    primeiro = mpatches.Patch(color='k', label=albuns[0])
-    segundo = mpatches.Patch(color='y', label=albuns[1])
-    terceiro = mpatches.Patch(color='g', label=albuns[2])
-    quarto = mpatches.Patch(color='r', label=albuns[3])
-    cor_leg = [primeiro, segundo, terceiro, quarto]
-
-    #criação dos gráficos
-    sns.set(style = 'whitegrid')
-
-    sns.barplot(x = df.index.levels[1].values, y = 'popularidade', data=df, palette=custom_palette)
-    plt.xticks(fontsize=7, rotation=80)
-    plt.title('Popularidade das músicas', fontsize=15)
-    plt.ylabel('Popularidade', fontsize=10)
-    plt.legend(handles=cor_leg)
-    plt.show()
-
-    sns.barplot(x = df.index.levels[1].values, y = 'duração(seg)', data=df, palette=custom_palette)
-    plt.xticks(fontsize=7, rotation=80)
-    plt.title('Durações das músicas', fontsize=15)
-    plt.ylabel('Duração(seg)', fontsize=10)
-    plt.legend(handles=cor_leg)
-    plt.show()
-
-    sns.barplot(data = df3, x = df3.index.values ,y = "prêmios")
-    plt.title('Prêmios dos álbuns', fontsize=15)
-    plt.xlabel('Álbuns', fontsize=10)
-    plt.ylabel('Prêmios', fontsize=10)
-    plt.show()
-
 #essa função plota graficos do tipo: mostre o maior tal e menor tal...
 def plot_mais_e_menos():
     lista = ['duração(seg)', 'popularidade']#se quiser mais alguma informação, adicionar aqui
@@ -199,10 +156,12 @@ def plot_mais_e_menos():
         for album, musicas in dict_albuns.items():
             custom_palette = []
             max_album = df.loc[album].idxmax()[coluna]
+            min_album = df.loc[album].idxmin()[coluna]
             for musica in musicas:
-
                 if musica == max_album:
                     custom_palette.append('r')
+                elif musica == min_album:
+                    custom_palette.append('b')
                 else:
                     custom_palette.append('k')
             sns.set(style = 'whitegrid')
@@ -211,4 +170,20 @@ def plot_mais_e_menos():
             plt.title(f'{coluna} em: {album}', fontsize=15)
             plt.ylabel(f'{coluna}', fontsize=10)
             plt.show()
-print(plot_mais_e_menos())
+    #plotando o gráfico dos prêmios
+    tuples = df.index.values
+    dict_albuns = {}
+    albuns = []
+    premios = []
+    for (key, value) in tuples:
+        dict_albuns.setdefault(key, []).append(df.loc[key].iloc[:, 13].sum())
+    for album in dict_albuns: 
+        albuns.append(album)
+    for p in dict_albuns.values():
+        premios.append(p[0])
+    sns.set(style = 'whitegrid')
+    sns.barplot(x = albuns, y = premios, data=df)
+    plt.xticks(fontsize=7)
+    plt.title("Prêmios por álbuns", fontsize=15)
+    plt.ylabel("Prêmios", fontsize=10)
+    plt.show()
