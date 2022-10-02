ff6c2215518f3036e6aaafec68eafb80593f8916
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-10-01 23:34:24 -0300

    Create get_lyrics.py

---
+++ get_lyrics.py
@@ -0,0 +1,54 @@
+import create_dataframe as create
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+
+# A url do genius segue o seguinte padrão: "https://genius.com/nome-do-artista-nome-da-música-lyrics". 
+# A função retornará a url de uma música específca ao receber o nome da música
+def formatar_para_url(nome_da_musica):
+    
+    # algumas musicas do album "An evening with silk sonic" do Bruno Mars
+    # aparecem no genius como musicas do silk song
+    chave = 0
+    if nome_da_musica in ['Silk Sonic Intro', 'Leave The Door Open', 'Fly As Me', 'After Last Night (with Thundercat & Bootsy Collins)',
+                            'Smokin Out The Window', 'Put On A Smile', '777', 'Skate', 'Blast Off']:
+        chave = 1
+    
+    #este trecho limpa pedaços como (feat. Damian Marley), deixando apenas o nome da musica
+    nome_da_musica = re.sub("\(.*?\)","",nome_da_musica)
+    
+    nome_da_musica = nome_da_musica.replace("&", "and").replace(" ", "-").lower().replace("'", "")
+    if nome_da_musica[-1] == '-':
+        nome_da_musica = nome_da_musica[:-1]
+    
+    if chave == 1:
+        return f'https://genius.com/Silk-sonic-{nome_da_musica}-lyrics'
+    return f'https://genius.com/Bruno-mars-{nome_da_musica}-lyrics'
+
+
+# Devolve a letra de uma música específica
+def get_lyrics(url):
+    soup = BeautifulSoup(requests.get(url).content, 'lxml')
+    letra=''
+    for tag in soup.select('div[class^="Lyrics__Container"], .song_body-lyrics p'):
+        t = tag.get_text(strip=True, separator='\n')
+        if t:
+            letra+=t
+    letra = letra.replace("\n", " ")
+    letra = re.sub("\[.*?\]", "", letra)
+    return letra
+
+#cria um json com a letra de todas as músicas
+def get_all_lyrics():
+    df = create.create_dataframe()
+    dic = {}
+    lista = []
+    for i in create.df.index.values:
+        url = formatar_para_url(i[1])
+        letra = get_lyrics(url)
+        dic = dict(album = f'{i[0]}', musica = f'{i[1]}', letra = f'{letra}')
+        lista.append(dic)
+    with open("musicas.json", "w") as outfile:
+        json.dump(lista, outfile)
