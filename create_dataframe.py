beb00b551b5671d544c73018f4012158ac8f05e1
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-10-04 19:38:45 -0300

    Update create_dataframe.py

--- create_dataframe.py
+++ create_dataframe.py
@@ -8,7 +8,7 @@ import numpy as np
 spot = spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials(client_id="4122b8a894694d3e8cf1d8a19cf93aec", client_secret="665e23d198b1458f8a4ce02303100b3d"))
 
 # no spotify, cada artista tem uma id para poder reconhecê-lo no sistema. 
-artist_id = '0du5cEVh5yTK9QJze8zA0C'#id do Bruno Mars
+artist_id = '0du5cEVh5yTK9QJze8zA0C' #id do Bruno Mars
 
 
 def get_all_albuns(id_artista):
@@ -18,13 +18,18 @@ def get_all_albuns(id_artista):
     :return: um dicionário com a id do album e o nome dele para cada album do artista
     :rtype: dict
     """
-    albuns_do_artista = spot.artist_albums(id_artista, album_type='album')
-    albuns = {}
-    for i in range(len(albuns_do_artista['items'])):
-        id = albuns_do_artista['items'][i]['id']
-        name = albuns_do_artista['items'][i]['name']
-        albuns[id] = name
-    return albuns
+
+    try:
+        albuns_do_artista = spot.artist_albums(id_artista, album_type='album')
+    except spotipy.exceptions.SpotifyException as error:
+        return error
+    else:
+        albuns = {}
+        for i in range(len(albuns_do_artista['items'])):
+            id = albuns_do_artista['items'][i]['id']
+            name = albuns_do_artista['items'][i]['name']
+            albuns[id] = name
+        return albuns
 
 
 def get_album_data(album_id, album_name):
@@ -39,24 +44,28 @@ def get_album_data(album_id, album_name):
     são são informações consideradas relevantes sobre cada música.
     :rtype: object
     """
-    tuple_index = []
-    data = []
-    tracks = spot.album_tracks(album_id)
-    for n in range(len(tracks['items'])):
-        id_track = tracks['items'][n]['id']
-        track = spot.track(id_track)
-        audio_features = spot.audio_features(id_track)[0]
-    
-        #construindo o data frame
-        tuple_index.append((album_name, track['name']))
-        data.append([int(round(track['duration_ms']/1000, 0)), track['popularity'], audio_features['danceability'], audio_features['energy'], audio_features['key'],
-        audio_features['loudness'], audio_features['mode'], audio_features['speechiness'], audio_features['acousticness'],audio_features['instrumentalness'], 
-        audio_features['liveness'], audio_features['valence'], audio_features['tempo']])
 
-    index = pd.MultiIndex.from_tuples(tuple_index, names=("Albuns", "Musics"))
+    try:
+        tuple_index = []
+        data = []
+        tracks = spot.album_tracks(album_id)
+        for n in range(len(tracks['items'])):
+            id_track = tracks['items'][n]['id']
+            track = spot.track(id_track)
+            audio_features = spot.audio_features(id_track)[0]
 
-    return pd.DataFrame(data, columns =['duration (sec)','popularity', 'danceability', 'energy', 'key', 'loudness', 
-    'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'], index=index)
+            #construindo o data frame
+            tuple_index.append((album_name, track['name']))
+            data.append([int(round(track['duration_ms']/1000, 0)), track['popularity'], audio_features['danceability'], audio_features['energy'], audio_features['key'],
+            audio_features['loudness'], audio_features['mode'], audio_features['speechiness'], audio_features['acousticness'],audio_features['instrumentalness'], 
+            audio_features['liveness'], audio_features['valence'], audio_features['tempo']])
+        
+        index = pd.MultiIndex.from_tuples(tuple_index, names=("Albuns", "Musics"))
+
+        return pd.DataFrame(data, columns =['duration (sec)','popularity', 'danceability', 'energy', 'key', 'loudness', 
+        'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'], index=index)
+    except Exception as error:
+        return error
 
 
 def create_dataframe():
@@ -67,18 +76,22 @@ def create_dataframe():
     são são informações consideradas relevantes sobre cada música.
     :rtype: object
     """
-    albuns = get_all_albuns(artist_id)
-    dfs ={}
-    for id, nome in albuns.items():
-        dfs[nome]= get_album_data(id, nome)
-    df = pd.concat(dfs.values())
+    
+    try:
+        albuns = get_all_albuns(artist_id)
+        dfs ={}
+        for id, nome in albuns.items():
+            dfs[nome]= get_album_data(id, nome)
+        df = pd.concat(dfs.values())
 
-    #tirando essa delux edition para nao ter músicas repetidas
-    df.drop('Unorthodox Jukebox (Deluxe Edition)', axis=0, level=0, inplace= True)
-    df.index = df.index.remove_unused_levels()
+        #tirando essa delux edition para nao ter músicas repetidas
+        df.drop('Unorthodox Jukebox (Deluxe Edition)', axis=0, level=0, inplace= True)
+        df.index = df.index.remove_unused_levels()
 
-    #adicionando a coluna prêmios pos música.Como são poucos prêmios, pegamos manualmente
-    df["prêmios"] = np.array([0,0,0,0,0,0,0,0,0,0,2,0,0,5,0,0,0,1,0,0,2,0,1,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0])
+        #adicionando a coluna prêmios pos música.Como são poucos prêmios, pegamos manualmente
+        df["prêmios"] = np.array([0,0,0,0,0,0,0,0,0,0,2,0,0,5,0,0,0,1,0,0,2,0,1,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0])
 
-    #df.to_csv(r'dataframe.csv')
-    return df
+        #df.to_csv(r'dataframe.csv')
+        return df
+    except Exception as error:
+        return error
