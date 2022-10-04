dcbca854462df77a0eec94420fddf7dd84baa85c
João Victor Pereira Oliveira <69165032+JoaoPereiraOliveira@users.noreply.github.com>
2022-10-03 21:31:57 -0300

    Update get_lyrics.py

--- get_lyrics.py
+++ get_lyrics.py
@@ -8,7 +8,13 @@ import re
 # A url do genius segue o seguinte padrão: "https://genius.com/nome-do-artista-nome-da-música-lyrics". 
 # A função retornará a url de uma música específca ao receber o nome da música
 def formatar_para_url(nome_da_musica):
+    """Recebe o nome de uma música e retorna sua url específica.
     
+    :param nome_da_musica: nome da música escolhida.
+    :type nome_da_musica: str
+    :return: a url de acesso da música escolhida.
+    :rtype: str
+    """
     # algumas musicas do album "An evening with silk sonic" do Bruno Mars
     # aparecem no genius como musicas do silk song
     chave = 0
@@ -30,6 +36,13 @@ def formatar_para_url(nome_da_musica):
 
 # Devolve a letra de uma música específica
 def get_lyrics(url):
+    """Recebe a url de uma música e retorna sua letra.
+    
+    :param url: url da música escolhida.
+    :type url: str
+    :return: letra completa da música escolhida.
+    :rtype: str
+    """
     soup = BeautifulSoup(requests.get(url).content, 'lxml')
     letra=''
     for tag in soup.select('div[class^="Lyrics__Container"], .song_body-lyrics p'):
@@ -42,6 +55,11 @@ def get_lyrics(url):
 
 #cria um json com a letra de todas as músicas
 def get_all_lyrics():
+    """Essa função não recebe parâmetros.
+
+        Essa função cria um json com a letra de todas as músicas.
+
+    """
     df = create.create_dataframe()
     dic = {}
     lista = []
