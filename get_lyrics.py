39bcae06a2351956a06cc50cf0c300b79d04bf7a
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-10-04 17:41:30 -0300

    Update get_lyrics.py

--- get_lyrics.py
+++ get_lyrics.py
@@ -5,8 +5,7 @@ import json
 import re
 
 
-# A url do genius segue o seguinte padrão: "https://genius.com/nome-do-artista-nome-da-música-lyrics". 
-# A função retornará a url de uma música específca ao receber o nome da música
+# A url do genius segue o seguinte padrão: "https://genius.com/nome-do-artista-nome-da-música-lyrics".
 def formatar_para_url(nome_da_musica):
     """Recebe o nome de uma música e retorna sua url específica.
     
@@ -15,6 +14,7 @@ def formatar_para_url(nome_da_musica):
     :return: a url de acesso da música escolhida.
     :rtype: str
     """
+    
     # algumas musicas do album "An evening with silk sonic" do Bruno Mars
     # aparecem no genius como musicas do silk song
     chave = 0
@@ -34,7 +34,6 @@ def formatar_para_url(nome_da_musica):
     return f'https://genius.com/Bruno-mars-{nome_da_musica}-lyrics'
 
 
-# Devolve a letra de uma música específica
 def get_lyrics(url):
     """Recebe a url de uma música e retorna sua letra.
     
@@ -53,13 +52,10 @@ def get_lyrics(url):
     letra = re.sub("\[.*?\]", "", letra)
     return letra
 
-#cria um json com a letra de todas as músicas
+
 def get_all_lyrics():
-    """Essa função não recebe parâmetros.
-
-        Essa função cria um json com a letra de todas as músicas.
-
-    """
+    """Essa função não recebe parâmetros e cria um json com a letra de todas as músicas. """
+    
     df = create.create_dataframe()
     dic = {}
     lista = []
